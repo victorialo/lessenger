b7b3c02e3a4a4270010a13fc13edecff6952bc5a
Victoria Lo <victoria@Victorias-MBP>
2017-10-15 00:30:47 -0700

    add color, and it now parses strings yay

--- lessenger_api/app/controllers/chat_controller.rb
+++ lessenger_api/app/controllers/chat_controller.rb
@@ -1,7 +1,13 @@
 class ChatController < ApplicationController
     # @users = {}
     def messages()
-        puts "#{params}"
+        puts "#{params}".red
+
+        queries = [
+            "what's the weather in",
+            "weather in",
+            "weather"
+        ]
         user = params[:user_id]
         if params[:name]
             # @users[user] = params[:name]
@@ -9,7 +15,24 @@ class ChatController < ApplicationController
         end
         if params[:text]
             msg = params[:text]
-            m = {type: 'text', text: msg}
+            reply = msg # parrot back what you say unless you're asking about weather
+            location = ""
+            queries.each_with_index do |q, ind|
+                next unless location == ""
+                if msg.include?(q)
+                    parse = msg.split(q).each{|phrase| phrase.strip}
+                    puts "#{parse}".yellow
+                    # if ind == 3
+                    #     location = parse[-1]
+                    # else
+                    location = parse[-1]
+                    # end
+                    reply = location
+                    puts "#{reply}".green
+                end
+            end
+            puts "#{reply}".magenta
+            m = {type: 'text', text: reply}
         end
         messages = [m]
         render json: {
