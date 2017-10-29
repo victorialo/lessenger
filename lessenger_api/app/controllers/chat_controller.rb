87e5a36914003f2ad29b401aa17661a4c3d113f8
Victoria Lo <victoria@Victorias-MBP>
2017-10-28 18:14:53 -0700

    add 'tomorrow' feature, some refactoring - work from the interview

--- lessenger_api/app/controllers/chat_controller.rb
+++ lessenger_api/app/controllers/chat_controller.rb
@@ -1,6 +1,9 @@
 class ChatController < ApplicationController
     # the query set to check text input with
     $QUERIES = [
+        "what's the weather tomorrow in",
+        "weather tomorrow in",
+        "weather tomorrow",
         "what's the weather in",
         "weather in",
         "weather"
@@ -56,6 +59,23 @@ class ChatController < ApplicationController
         return $WEATHER[icon]
     end
 
+    # generate actual response to speaker
+    # @param address [String] location of the weather
+    # @param weather [Hash] Hash table of parsed JSON of information
+    # @param tomorrow [Boolean] whether you want the current weather or the weather tomorrow
+    def weather_response(address, weather, tomorrow=false)
+        if tomorrow
+            data = weather['daily']['data'][1]
+            return "Tomorrow, the weather in <b>#{address}</b> is <i>#{data['summary'].downcase}</i>, with a temperature low of #{data['temperatureMin']}°F (feeling like #{data['apparentTemperatureMin']}°F) and a temperature high of #{data['temperatureMax']}°F (feeling like #{data['apparentTemperatureMax']}°F). There is a #{data['precipProbability'].to_f*100}% chance of rain.<BR><BR>
+            Have a good day!"
+        else
+            return "Currently, the weather in <b>#{address}</b> is <i>#{weather['currently']['summary'].downcase}</i>, with a temperature of #{weather['currently']['temperature']}°F (feeling like #{weather['currently']['apparentTemperature']}°F). There is a #{weather['currently']['precipProbability'].to_f*100}% chance of rain.<BR><BR>
+            Today, expect <i>#{weather['hourly']['summary'].downcase}</i><BR>
+            Over the week, expect <i>#{weather['daily']['summary'].camelize(:lower)}</i><BR><BR>
+            Have a good day!"
+        end
+    end
+
     # the main function to handle input
     # params are already passed in automatically
     # @output [Hash] the reply, following the lessenger API
@@ -68,9 +88,10 @@ class ChatController < ApplicationController
         end
         if params[:text]
             msg = params[:text]
-            reply = "<i>#{msg}</i>" # parrot back what you say unless you're asking about weather
             location = ""
+            reply = ""
             $QUERIES.each_with_index do |q, ind|
+                tomorrow = q.include?('tomorrow')
                 next unless location == ""
                 if msg.include?(q)
                     parse = msg.split(q).each{|phrase| phrase.strip}
@@ -79,6 +100,7 @@ class ChatController < ApplicationController
                     coords = loc_json(location)
                     if coords[:coords].empty?
                         reply = "Sorry, that location (#{location}) doesn't seem to exist. Try again?"
+                        messages << {type: 'rich', html: reply}
                         next
                     else
                         loc = coords[:coords]
@@ -91,13 +113,14 @@ class ChatController < ApplicationController
                         type: 'rich',
                         html: "<iframe src='#{icon(weather['currently']['icon'])}'></iframe>"
                     }
-                    reply = "Currently, the weather in <b>#{address}</b> is <i>#{weather['currently']['summary'].downcase}</i>, with a temperature of #{weather['currently']['temperature']}°F (feeling like #{weather['currently']['apparentTemperature']}°F). There is a #{weather['currently']['precipProbability'].to_f*100}% chance of rain.<BR><BR>
-                    Today, expect <i>#{weather['hourly']['summary'].downcase}</i><BR>
-                    Over the week, expect <i>#{weather['daily']['summary'].camelize(:lower)}</i><BR><BR>
-                    Have a good day!"
+                    reply = weather_response(address, weather, tomorrow)
+                    messages << {type: 'rich', html: reply}
                 end
             end
-            messages << {type: 'rich', html: reply}
+            if reply.empty? # parrot back what you say unless you're asking about weather
+                reply = "<i>#{msg}</i>"
+                messages << {type: 'rich', html: reply}
+            end
         end
         render json: {
             success: 'success',
